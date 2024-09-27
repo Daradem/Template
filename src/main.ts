35896f564d55c0eedb82ebb6457e05a6c9cf260e
Mauricio Portugués Castellanos <Mauricio.Portugues.Castellanos@outlook.com>
2024-09-27 16:30:50 -0600

    feat: ✨ Update main web server files content

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { appConfig } from './app/application/config';
+import { AppComponent } from './app/presenters/components/home/home';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
