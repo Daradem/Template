a013cde17e5c543feaa73d254876f3ff8a0d7ce7
Mauricio Portugués Castellanos <Mauricio.Portugues.Castellanos@outlook.com>
2024-09-27 16:30:30 -0600

    feat: ✨ Update home components content to adapt them into the hexagonal architecture

--- src/app/presenters/components/home/home.ts
+++ src/app/presenters/components/home/home.ts
@@ -5,8 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  templateUrl: './home.html',
+  styleUrl: './home.scss'
 })
 export class AppComponent {
   title = 'popotillo-eshop';
